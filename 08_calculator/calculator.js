caed3f0add24a0cd6b28b119937ee65414eeb94f
Steven <stevenescobar131@gmail.com>
2022-08-15 18:13:32 -0500

    working on exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a = []) {
+  return a.reduce( (total, b) => total + b,0);
 };
 
-const multiply = function() {
-
+const multiply = function(a=[]) {
+  return a.reduce( (total, b) => total * b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let j = 1;
+	for (let i = 1; i <= a; i++) {
+     j *= i;
+  }
+  return j;
 };
 
 // Do not edit below this line
